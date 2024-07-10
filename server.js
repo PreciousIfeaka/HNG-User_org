84c183aabb6105a227e30a3f56e153c918a13ce8
PreciousIfeaka <preciousifeaka@gmail.com>
2024-07-10 06:09:28 +0100

    corrected swagger setting

--- server.js
+++ server.js
@@ -30,7 +30,13 @@ const CSS_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const swaggerDocument = require("./swagger-output.json");
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    customCss:
+      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+    customCssUrl: CSS_URL }));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
