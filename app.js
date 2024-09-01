b9244b1f8ae3b050bf033e3559cecf8c3bd787b4
PreciousIfeaka <preciousifeaka@gmail.com>
2024-09-01 07:55:31 +0100

    fix: swagger documentation fix

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {customCssUrl: process.env.CSS_URL}));
 
 app.use("/auth", registerRouter);
 
